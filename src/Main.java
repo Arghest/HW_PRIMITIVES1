177ae91e057e7ce7f167318beddce4bc01ca8293
Arghest <bastet.wilhelm0@gmail.com>
2023-08-12 17:24:18 +0700

    init

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 1050;
+
+        int percent = amount / 20;
+
+        System.out.println(percent);
+
+
+    }
+}
